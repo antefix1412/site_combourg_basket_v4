fa611dfc9c6f91de281061b7cac08f7cd45af982
Antoine Lemesle <a.lemesle26@gmail.com>
2025-03-03 11:59:46 +0100

    ballon supr

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ export default function Home() {
             <li>🤝 Entraide</li>
             <li>🎖️ Respect</li>
           </ul>
-          <BouncingBasketball/>
         </div>
         
         
