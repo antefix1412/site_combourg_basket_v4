817284b3df0201e013037874158bb6d39a6b128f
Antoine Lemesle <a.lemesle26@gmail.com>
2025-03-04 12:39:18 +0100

    modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- next.config.mjs
+++ next.config.mjs
@@ -6,30 +6,5 @@ const nextConfig = {
   },
 };
 
-export default {
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Appliquer à toutes les routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline'",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-        ],
-      },
-    ];
-  },
-};
+export default nextConfig;
+
